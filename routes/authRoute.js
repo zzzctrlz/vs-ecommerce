cf8fab0168e224a706b89a1ee2cdb999c9d38902
Max <Maxthemaker8@gmail.com>
2022-09-28 17:54:15 -0700

    remove node modules and .env, add .gitignore

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,17 @@
 const router = require("express").Router();
 const cryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User")
+const User = require("../models/User");
+
+//REGISTER
+
+router.post("/register", async(req, res) => {
+   const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: CryptoJS.AES.encrypt(
+         req.body.password,
+         process.env.SPW
+      )
+   });
+});
