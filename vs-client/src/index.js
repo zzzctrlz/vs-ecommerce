24e895adc507ce0642706aa77d02d28d6cddc2fe
Max <Maxthemaker8@gmail.com>
2022-12-23 13:22:45 -0800

    update client index.js w redux

--- vs-client/src/index.js
+++ vs-client/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-//import './index.css';
+import ReactDOM from 'react-dom'; //changed from 'from 'react-dom/client' to just react-dom
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {store,persistor} from "./redux/store";
+import {PersistGate} from "redux-persist/integration/react";
 
+// NEWER SYNTAX (ReactDOM.render now legacy)
+//not sure if need to include <React.StrictMode>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+         <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
-);
+); 
+
+
+/* LEGACY
+ReactDOM.render(
+   <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+         <App />
+      </PersistGate>
+   </Provider>,
+   document.getElementById("root")
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
